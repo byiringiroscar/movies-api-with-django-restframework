1a0f570e3d7b6d2800c8dbd53c8300296477ff7d
byiringiroscar <byiringoroscar@gmail.com>
2023-04-23 00:30:19 +0200

    Add path for create movie

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('movie/', get_all_movies, name="get-all-movie"),
+    path('movie/', create_movie, name="get-all-movie"),
     path('movie/<id>/', get_movie, name="get-movie"),
     path('movie/<id>/', update_movie, name="update-movie"),
     path('movie/<id>/', delete_movie, name="delete-movie"),
