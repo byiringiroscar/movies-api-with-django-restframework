ad46e0713959742478411cbccc876609d227e0d7
byiringiroscar <byiringoroscar@gmail.com>
2023-04-22 23:55:02 +0200

    Create serializer class for Movie model

---
+++ home/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Movies
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movies
+        fields = ['name', 'image', 'movie_link', ]
