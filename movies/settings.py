317c204abb73c7910ce4a9f9d4e0d28a5aad82fe
byiringiroscar <byiringoroscar@gmail.com>
2023-04-22 23:45:04 +0200

    setu django rest framework

--- movies/settings.py
+++ movies/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
