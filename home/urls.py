fa68d3ba4c6acdddd9e9933e7068a924d0e2d544
byiringiroscar <byiringoroscar@gmail.com>
2023-04-23 00:27:35 +0200

    Add create, get, update and delete enpoint for movie API

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('movie/<id>/', get_movie, name="get-movie")
+    path('movie/', get_all_movies, name="get-all-movie"),
+    path('movie/<id>/', get_movie, name="get-movie"),
+    path('movie/<id>/', update_movie, name="update-movie"),
+    path('movie/<id>/', delete_movie, name="delete-movie"),
 ]
--- home/views.py
+++ home/views.py
@@ -14,8 +14,56 @@ from .serializers import MovieSerializer
 def get_movie(request, id):
     try:
         movie = Movies.objects.get(id=id)
-    except:
+    except Movies.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
+
+
+@api_view(['PUT', ])
+def update_movie(request, id):
+    try:
+        movie = Movies.objects.get(id=id)
+    except Movies.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        serializer = MovieSerializer(movie, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["success"] = "update successful"
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', ])
+def delete_movie(request, id):
+    try:
+        movie = Movies.objects.get(id=id)
+    except Movies.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'DELETE':
+        operation = movie.delete()
+        data = {}
+        if operation:
+            data["success"] = "delete successful"
+        else:
+            data['failure'] = "delete failed"
+        return Response(data=data)
+
+
+@api_view(['GET', ])
+def get_all_movies(request):
+    movies = Movies.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST', ])
+def create_movie(request):
+    serializer = MovieSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
