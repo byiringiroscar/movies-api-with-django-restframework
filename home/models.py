a92183e568ffea61e51577f53ec69c009f3eed1a
byiringiroscar <byiringoroscar@gmail.com>
2023-04-22 23:50:42 +0200

    Add movies model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Movies(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+    movie_link = models.URLField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = ['Movies']
