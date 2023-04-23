16422154a61d7d9bbffb585b3a35df064c664136
byiringiroscar <byiringoroscar@gmail.com>
2023-04-23 12:14:01 +0200

    Add category attribute in moves

--- home/models.py
+++ home/models.py
@@ -4,9 +4,19 @@ from django.db import models
 # Create your models here.
 
 class Movies(models.Model):
+    category = [
+        ('action', 'action'),
+        ('blockbuster', 'blockbuster'),
+        ('horror', 'horror'),
+        ('comedian', 'comedian'),
+        ('thriller', 'thriller'),
+        ('romance', 'romance'),
+
+    ]
     name = models.CharField(max_length=100)
     image = models.ImageField()
     movie_link = models.URLField()
+    movie_type = models.CharField(max_length=200, choices=category, default='action')
     date_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
